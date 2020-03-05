f4cb818863cf186f2d0c56e55ef8d27ae1964076
seank329 <seankamran@gmail.com>
2020-03-05 13:59:46 -0500

    Updated Portfolio with Battleship and Memory Challenge

--- src/information.js
+++ src/information.js
@@ -2,26 +2,28 @@ import quizApp from './images/quizApp.png';
 import starWars from './images/starWars.png';
 import petful from './images/petful.png';
 import spacedRep from './images/spacedRep.png';
+import battleship from './images/battleship.png'
+import memory from './images/memory.png'
 
 export default [
 
    {
-       title: 'Quiz App',
-       imageURL: quizApp,
-       imageAlt:'Quiz App',
-       description: `An application designed to test the user's knowledge of country capitals`,
-       techList: 'HTML, CSS, JavaScript, jQuery',
-       linkLive: 'https://seank329.github.io/',
-       linkRepo: 'https://github.com/thinkful-ei-gecko/Sean-QuizAppSolo'
+        title: 'Battleship',
+        imageURL: battleship,
+        imageAlt: 'Battleship',
+        description:'Recreation of the iconic two-player boardgame, with live-chat, the ability to resume games, and game-history.',
+        techList:'ReactJS, HTML, CSS, JavaScript, NodeJS, ExpressJS, Socket.IO, PostgreSQL',
+        linkLive:'https://sean-battleship-app.now.sh/',
+        linkRepo:'https://github.com/seank329/Battleship-Client.git, https://github.com/seank329/Battleship-Server.git'
    },
    {
-       title: 'Star Wars API Search',
-       imageURL: starWars,
-       imageAlt: 'Star Wars',
-       description: `A front-end allowing users to search the Star Wars API (SWAPI)`,
-       techList: 'HTML, CSS, JavaScript, ReactJS',
-       linkLive: 'https://sean-star-wars-api.now.sh/',
-       linkRepo:'https://github.com/thinkful-ei-heron/StarWars-Sean.git'
+        title: 'Memory Challenge',
+        imageURL: memory,
+        imageAlt: 'Memory Challenge',
+        description:'Find matching pairs of cards on a randomly sorted gameboard. Multiple difficulty levels, login to save progress',
+        techList:'ReactJS, HTML, CSS, JavaScript, NodeJS, ExpressJS, jwt.io (with token expiry), PostgreSQL',
+        linkLive:'https://memory-app-sigma.now.sh/',
+        linkRepo:'https://github.com/seank329/Capstone1-UI.git, https://github.com/seank329/Capstone1-Server.git'
    },
    {
        title: 'Petful',
@@ -30,7 +32,7 @@ export default [
        description: `A mock website for an animal shelter allowing users to adopt animals. Queues mimic adoption process with it's "FIFO" business model.`,
        techList:'ReactJS, HTML, CSS, JavaScript, NodeJS, ExpressJS',
        linkLive: 'https://petful-client-sean-andrea.andreabender.now.sh/',
-       linkRepo: 'https://github.com/thinkful-ei-heron/Petful-Server-Sean-Andrea, https://github.com/thinkful-ei-heron/Petful-Client-Sean-Andrea'
+       linkRepo: 'https://github.com/seank329/Petful-Server.git, https://github.com/seank329/Petful-Client.git'
    },
    {
        title: 'German Language with Spaced Repetition',
@@ -39,8 +41,26 @@ export default [
        description: `Full-stack implementation of the spaced-repetition language learning technique mimicking flashcards. Utilizes linked-list data type.`,
        techList: 'ReactJS, CSS, JavaScript, NodeJS, ExpressJS, PostgreSQL',
        linkLive: 'https://spaced-repetition-client.rbannal.now.sh/register',
-       linkRepo:'https://github.com/thinkful-ei-heron/spaced-repetition-api-alex-sean, https://github.com/thinkful-ei-heron/spaced-repetition-client-alex-sean'
+       linkRepo:'https://github.com/seank329/SpacedRepetition-Server.git, https://github.com/seank329/SpacedRepetition-Client.git'
 
+   },
+   {
+        title: 'Star Wars API Search',
+        imageURL: starWars,
+        imageAlt: 'Star Wars',
+        description: `A front-end allowing users to search the Star Wars API (SWAPI)`,
+        techList: 'HTML, CSS, JavaScript, ReactJS',
+        linkLive: 'https://sean-star-wars-api.now.sh/',
+        linkRepo:'https://github.com/seank329/StarWars-Api.git'
+   },
+   {
+        title: 'Quiz App',
+        imageURL: quizApp,
+        imageAlt:'Quiz App',
+        description: `An application designed to test the user's knowledge of country capitals`,
+        techList: 'HTML, CSS, JavaScript, jQuery',
+        linkLive: 'https://seank329.github.io/',
+        linkRepo: 'https://github.com/seank329/QuizApp.git'
    }
 
 
